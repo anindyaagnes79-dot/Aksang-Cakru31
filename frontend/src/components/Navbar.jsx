b470490268e7ba5d29ccf73d37274b14dc58d01f
waw <anindyaagnes79@gmail.com>
2026-01-01 12:44:30 +0700

    konfigurasi livehost

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+            <div>
+        <h2>HEARMONY</h2>
+    </div>
+    <div>
+        <u1>
+            <li>HOME</li>
+            <li>RESERVATION</li>
+            <li>MERCHANDISE</li>
+        </u1>
+    </div>
+    </div>
+  )
+};
+
+export default Navbar;
