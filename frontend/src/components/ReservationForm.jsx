e8bcac457cad600360284ee8568b71d0a272b58d
waw <anindyaagnes79@gmail.com>
2026-01-11 17:39:02 +0700

    rsvp fix

--- frontend/src/components/ReservationForm.jsx
+++ frontend/src/components/ReservationForm.jsx
@@ -1,30 +1,14 @@
 function ReservationForm() {
   return (
-    <section
-      className="
-        min-h-screen
-        bg-black text-white
-        pt-[80px]
-        pb-40
-      "
-    >
-      <div className="max-w-5xl mx-auto">
-        
-     <iframe
-      id="JotFormIFrame-260093924222452"
-      title="HEARMONY RESERVATION"
-      onload="window.parent.scrollTo(0,0)"
-      allowtransparency="true"
-      allow="geolocation; microphone; camera; fullscreen; payment"
-      src="https://form.jotform.com/260093924222452"
-      frameborder="0"
-      style="min-width:100%;max-width:100%;height:539px;border:none;"
-      scrolling="no"
-     >
-     </iframe>
-    <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
-    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-260093924222452']", "https://form.jotform.com/")</script>
-    
+    <section className="min-h-screen bg-black text-white pt-[80px] pb-40">
+      <div className="max-w-5xl mx-auto min-h-[900px]">
+        <iframe
+          title="HEARMONY RESERVATION"
+          src="https://form.jotform.com/260093924222452"
+          className="w-full min-h-[900px] border-none"
+          allow="geolocation; microphone; camera; fullscreen; payment"
+          scrolling="auto"
+        />
       </div>
     </section>
   );
