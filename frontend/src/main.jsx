370c85f880d29a0322b99b8b0b67a1dcfe2b5fba
waw <anindyaagnes79@gmail.com>
2026-01-13 13:46:51 +0700

    add react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import {BrowserRouter} from "react-router-dom";
 import {Analytics} from "@vercel/analytics/react";
-import {SpeedInsights} from "@vercel/speed-insights"
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
